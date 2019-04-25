bb82066fc54bb162bb1bd6e60a0604cd8fac5b37
Ondrej Filip <ondrej94filip@gmail.com>
2019-04-25 18:31:51 +0200

    second commit

--- task.py
+++ task.py
@@ -1,51 +1,112 @@
-# Class diary
-#
-# Create program for handling lesson scores.
-# Use python to handle student (highscool) class scores, and attendance.
-# Make it possible to:
-# - Get students total average score (average across classes)
-# - get students average score in class
-# - hold students name and surname
-# - Count total attendance of student
-# The default interface for interaction should be python interpreter.
-# Please, use your imagination and create more functionalities.
-# Your project should be able to handle entire school.
-# If you have enough courage and time, try storing (reading/writing)
-# data in text files (YAML, JSON).
-# If you have even more courage, try implementing user interface.
-#
-#Try to expand your implementation as best as you can.
-#Think of as many features as you can, and try implementing them.
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
-#
-#Your program must be runnable with command "python task.py".
-#Show some usecases of your library in the code (print some things)
-#
-#When you are done upload this code to your github repository.
-#
-#Delete these comments before commit!
-#Good luck.
-class student:
+import json
+import copy
 
-    def __init__(self, firstname, surname):
-        self.firstname = firstname
-        self.surname = surname
 
-student1 = student('John','Doe')
-student2 = student('Franta','Lala')
+def get_student_average(student, classrooms):
+    count = 0
+    i = 1
+    for room in classrooms:
+        count += room.get_student_average(student)
+        i += 1
+    return count/i
 
-class diary(student):
 
-    def __init__(self, score, attendance):
-        super().__init__(firstname, surname)
-        self.score = score
-        self.attendance = attendance
+class Student:
 
-    def averageinclass(self):
-        return sum(self.score) / len(self.score)
+    def __init__(self, student_id, first_name, last_name):
+        self.id = student_id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.score = []
+        self.attendance = 0
 
-    def averagetotal(self):
-        pass
+    def filter_score(self):
+        return list(filter(lambda x: x >= 50, self.score))
+
+    def __repr__(self):
+        return '{} {}'.format(self.first_name, self.last_name)
+
+
+class ClassRoom:
+
+    def __init__(self, classroom_name):
+        self.classroom_name = classroom_name
+        self.students = []
+
+    def add_student(self, student):
+        self.students.append(student)
+
+    def add_score(self, student_id, score):
+        for item in self.students:
+            if item.id == student_id:
+                item.score.append(score)
+
+    def add_attendance(self, student_id):
+        for item in self.students:
+            if item.id == student_id:
+                item.attendance += 1
+
+    def get_student_average(self, student_id):
+        for student in self.students:
+            if student.id == student_id:
+                return sum(student.score) / len(student.score)
+        return 0
+
+    def __repr__(self):
+        return '{} {}'.format(self.classroom_name, self.students)
+
+
+def main():
+    classrooms_list = []
+
+    classroom1 = ClassRoom('Python')
+    classroom2 = ClassRoom('Math')
+
+    classrooms_list.append(classroom1)
+    classrooms_list.append(classroom2)
+
+    student1 = Student('s1', 'John', 'Doe')
+    student2 = Student('s2', 'Franta', 'Lala')
+    student3 = Student('s3', 'Alan', 'Turing')
+
+    classroom1.add_student(student1)
+    classroom1.add_student(student2)
+    classroom2.add_student(student3)
+    classroom2.add_student(copy.deepcopy(student1))
+
+    classroom1.add_score(student1.id, 40)
+    classroom1.add_score(student1.id, 60)
+    classroom1.add_score(student1.id, 70)
+    classroom2.add_score(student1.id, 40)
+    classroom2.add_score(student1.id, 50)
+
+    classroom1.add_attendance(student1.id)
+    classroom1.add_attendance(student1.id)
+    classroom2.add_attendance(student1.id)
+
+    print('ClassRoom1 Student1 average: {}'.format(classroom1.get_student_average(student1.id)))
+    print('Student1 All ClassRooms average: {}'.format(get_student_average(student1.id, classrooms_list)))
+    print('Student1 filter score above average: {}'.format(student1.filter_score()))
+
+    data = {}
+    data['Students'] = []
+
+    for classroom in classrooms_list:
+
+        for item in classroom.students:
+            data['Students'].append({
+                'Classroom Name': classroom.classroom_name,
+                'First Name': item.first_name,
+                'Last Name': item.last_name,
+                'Score': item.score,
+                'Attendance': item.attendance
+            })
+
+    file = open('report.json', 'w', encoding='utf-8')
+    json.dump(data, file, indent=2, ensure_ascii=False)
+    print('Data save to file.')
+    file.close()
+
+
+if __name__ == '__main__':
+    main()
